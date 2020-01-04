b094774082ff8ae732a95224c253644ee10909e2
Mikael Korpela <mikael@ihminen.org>
2020-01-04 21:18:56 +0200

    Revert "Update AboutMe.component.js"
    This reverts commit fef3e8dd8f8f1188eca8ed8ad215965adc839f89.

--- modules/users/client/components/AboutMe.component.js
+++ modules/users/client/components/AboutMe.component.js
@@ -1,5 +1,3 @@
-/** @format */
-
 import React, { Component } from 'react';
 import { withTranslation } from '@/modules/core/client/utils/i18n-angular-load';
 import '@/config/client/i18n';
@@ -9,88 +7,57 @@ import { limitTo, plainTextLength } from '@/modules/core/client/utils/filters';
 export class AboutMe extends Component {
   constructor(props) {
     super(props);
-    this.changeProfileDescriptionToggle = this.changeProfileDescriptionToggle.bind(
-      this,
-    );
+    this.changeProfileDescriptionToggle = this.changeProfileDescriptionToggle.bind(this);
 
     this.state = {
       profileDescriptionToggle: false,
     };
   }
-  changeProfileDescriptionToggle() {
-    this.setState(prevState => ({
+  changeProfileDescriptionToggle(){
+    this.setState((prevState) => ({
       profileDescriptionToggle: !prevState.profileDescriptionToggle,
     }));
   }
 
-  render() {
+  render(){
     const { t, profile, isSelf, appSettings } = this.props;
-    return (
-      <>
-        <section className="panel panel-default">
-          <header className="panel-heading">{t('About me')}</header>
-          <div className="panel-body">
-            {/* Short descriptions */}
-            {(profile.description.length < 2400 ||
-              this.state.profileDescriptionToggle) && (
-              <div
-                dangerouslySetInnerHTML={{ __html: profile.description }}
-              ></div>
-            )}
+    return (<>
+      <section className="panel panel-default">
+        <header className="panel-heading">
+          {t('About me')}
+        </header>
+        <div className="panel-body">
+          {/* Short descriptions */}
+          {(profile.description.length < 2400 || this.state.profileDescriptionToggle) &&
+          <div dangerouslySetInnerHTML={{ __html: profile.description }}></div>}
 
-            {/* Long descriptions */}
-            {profile.description.length >= 2400 &&
-              !this.state.profileDescriptionToggle && (
-                <div className="panel-more-wrap">
-                  <div
-                    className="panel-more-excerpt"
-                    dangerouslySetInnerHTML={{
-                      __html: limitTo(profile.description, 2400),
-                    }}
-                  ></div>
-                  <div
-                    className="panel-more-fade"
-                    onClick={this.changeProfileDescriptionToggle}
-                  >
-                    {t('Show more...')}
-                  </div>
-                </div>
-              )}
+          {/* Long descriptions */}
+          {profile.description.length >= 2400 && !this.state.profileDescriptionToggle &&
+          <div
+            className="panel-more-wrap">
+            <div className="panel-more-excerpt" dangerouslySetInnerHTML={{ __html: limitTo(profile.description, 2400) }}></div>
+            <div className="panel-more-fade" onClick={this.changeProfileDescriptionToggle}>{t('Show more...')}</div>
+          </div>}
 
-            {/* If no description, show deep thoughts... */}
-            {(!profile.description || profile.description === '') && (
-              <blockquote
-                className="profile-quote"
-                aria-label={t(
-                  'Member has not written description about themself.',
-                )}
-              >
-                “Everyone is necessarily the hero of their own life story.”
-              </blockquote>
-            )}
-          </div>
+          {/* If no description, show deep thoughts... */}
+          {(!profile.description || profile.description === '') &&
+          <blockquote className="profile-quote"
+            aria-label={t('Member has not written description about themself.')}>
+          “Everyone is necessarily the hero of their own life story.”
+          </blockquote>}
+        </div>
 
-          {/* User watching their own profile and it's too short */}
-          {isSelf &&
-            (!profile.description ||
-              plainTextLength(profile.description) <
-                appSettings.profileMinimumLength) && (
-              <footer className="panel-footer">
-                <p className="lead">
-                  {t(
-                    'Your profile description should be longer so that you can send messages.',
-                  )}
-                  <br />
-                  <br />
-                  <a href="/profile/edit" className="btn btn-primary">
-                    {t('Fill your profile')}
-                  </a>
-                </p>
-              </footer>
-            )}
-        </section>
-      </>
-    );
+        {/* User watching their own profile and it's too short */}
+        {(isSelf && (!profile.description || plainTextLength(profile.description) < appSettings.profileMinimumLength)) &&
+        <footer className="panel-footer">
+          <p className="lead">
+            {t('Your profile description should be longer so that you can send messages.')}
+            <br/><br />
+            <a href="/profile/edit" className="btn btn-primary">{t('Fill your profile')}</a>
+          </p>
+        </footer>}
+      </section>
+    </>);
   }
 }
 
